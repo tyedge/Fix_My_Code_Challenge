d8e00ceccd8c50b1cd7ca8e59532b4a52a2aac10
tyedge <tyedge.inc@gmail.com>
2019-10-06 06:05:38 +0000

    Added documentation, made pep8 compliant

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
+""" This module defines the Square class """
 
-class square():
+
+class Square():
 
     width = 0
     height = 0
 
-
     def __init__(self, *args, **kwargs):
+        """ New instance of a square """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -15,14 +17,15 @@ class square():
         return self.width * self.height
 
     def PermiterOfMySquare(self):
+        """ Perimeter of the square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ Defines the str method for an instance of the square """
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
-
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
