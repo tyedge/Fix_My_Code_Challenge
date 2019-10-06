01fe6fe34bee59e2d6ba6e8658c00fd298d6bb28
tyedge <tyedge.inc@gmail.com>
2019-10-06 07:22:12 +0000

    Added list and if statement

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -10,17 +10,22 @@ class Square():
 
     def __init__(self, *args, **kwargs):
         """ New instance of a square """
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+        new = ['width', 'height']
+        if not args:
+            for key, value in kwargs.items():
+                if key in new:
+                    setattr(self, key, value)
+        """
         if kwargs['width'] == kwargs['height']:
             self.width = width
             self.height = height
+        """
 
     def area_of_my_square(self):
         """ Area of the square """
         return self.width * self.height
 
-    def PermiterOfMySquare(self):
+    def perimeter_of_my_square(self):
         """ Perimeter of the square """
         return (self.width * 2) + (self.height * 2)
 
@@ -32,4 +37,4 @@ if __name__ == "__main__":
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.perimeter_of_my_square())
